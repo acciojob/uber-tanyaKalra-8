108954b85c352b867bc74023c71e4daefb30f6d0
Taniya <tanya.kalra200@gmail.com>
2023-03-18 15:55:04 +0530

    Uber

---
+++ src/main/java/com/driver/model/Cab.java
@@ -0,0 +1,59 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cab {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private int perKmRate;
+    private boolean available;
+
+    @OneToOne
+    @JoinColumn
+    Driver driver;
+
+    public Cab() {
+    }
+
+    public Cab(int id, int perKmRate, boolean available, Driver driver) {
+        Id = id;
+        this.perKmRate = perKmRate;
+        this.available = available;
+        this.driver = driver;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
